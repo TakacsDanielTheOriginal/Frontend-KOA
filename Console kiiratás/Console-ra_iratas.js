ea949aaad33d0448ff9df491cb0bc1dc3f34c26e
TakacsDanielTheOriginal <sz4takdan@vasvari.org>
2025-09-08 10:22:02 +0200

    Add files via upload

---
+++ Console kiiratás/Console-ra_iratas.js
@@ -0,0 +1,9 @@
+function irat(){
+    let nev = document.getElementById("nev").value
+    let telepules = document.getElementById("telep").value
+    let iranyito = document.getElementById("iranyito").value
+    let kozjelleg = document.getElementById("kozjel").value
+    let hazszam = document.getElementById("hazszam").value
+
+    console.log(nev,telepules,iranyito,kozjelleg,hazszam)
+}
